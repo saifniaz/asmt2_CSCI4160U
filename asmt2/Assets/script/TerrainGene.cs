8d1b6511b007a70d6e6aa95cefb5a27f2ff22990
Saif Niaz <saifniaz@Saifs-MacBook-Pro.local>
2019-03-14 12:21:12 -0400

    Uploading skeleton version of the assignment

---
+++ asmt2/Assets/script/TerrainGene.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class TerrainGene : MonoBehaviour
+{
+    public int width = 256;
+    public int height = 256;
+    public int depth = 0;
+
+    public float scale = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Terrain terrain = GetComponent<Terrain>();
+        terrain.terrainData = generateTerrain(terrain.terrainData);
+    }
+
+    TerrainData generateTerrain(TerrainData terrainData) {
+        terrainData.heightmapResolution = width + 1;
+        terrainData.size = new Vector3(width, depth, height);
+        terrainData.SetHeights(0, 0, generateHeight());
+        return terrainData;
+    }
+
+    float[,] generateHeight() {
+        float[,] heights = new float[width, height];
+        for (int a = 0; a < width; a++) {
+            for (int b = 0; b < height; b++){
+                heights[a, b] = calculateHeight(a, b);
+            }
+        }
+        return heights;
+    }
+
+    float calculateHeight(int a, int b) {
+        float xCoord = (float) a / width * scale;
+        float yCoord = (float) b / height * scale;
+
+        return Mathf.PerlinNoise(xCoord, yCoord); 
+    }
+}
